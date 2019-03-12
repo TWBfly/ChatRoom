b958a317d2e45c531426dc2cdfd25b4b9b27e232
tang <1063351761@qq.com>
2019-03-12 22:19:36 +0800

    add:优化Thread

---
+++ sample-server/src/main/java/net/qiujuer/lesson/sample/server/TCPServer.java
@@ -0,0 +1,164 @@
+package net.qiujuer.lesson.sample.server;
+
+import net.qiujuer.lesson.sample.server.handle.ClientHandler;
+import net.qiujuer.library.clink.utils.CloseUtils;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.channels.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TCPServer implements ClientHandler.ClientHandlerCallback {
+    private final int port;
+    private ClientListener mListener;
+    private List<ClientHandler> clientHandlerList = new ArrayList<>();
+    private final ExecutorService forwardingThreadPoolExecutor;
+    private Selector selector;
+    private ServerSocketChannel server;
+
+    public TCPServer(int port) {
+        this.port = port;
+        //转发线程池
+        forwardingThreadPoolExecutor = Executors.newSingleThreadExecutor();
+    }
+
+    public boolean start() {
+        try {
+            selector = Selector.open();
+            server = ServerSocketChannel.open();
+            server.configureBlocking(false);//设置非阻塞
+            server.socket().bind(new InetSocketAddress(port));//绑定本地端口
+            server.register(selector, SelectionKey.OP_ACCEPT);//注册客户端连接到达监听
+            System.out.println("服务器信息：" + server.getLocalAddress().toString());
+            //启用客户端监听
+            ClientListener listener = new ClientListener();
+            mListener = listener;
+            listener.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public void stop() {
+        if (mListener != null) {
+            mListener.exit();
+        }
+        CloseUtils.close(server);
+        CloseUtils.close(selector);
+
+        synchronized (TCPServer.this) {
+            for (ClientHandler clientHandler : clientHandlerList) {
+                clientHandler.exit();
+            }
+            clientHandlerList.clear();
+        }
+        //停止线程池
+        forwardingThreadPoolExecutor.shutdownNow();
+    }
+
+    public synchronized void broadcast(String str) {
+        for (ClientHandler clientHandler : clientHandlerList) {
+            clientHandler.send(str);
+        }
+    }
+
+    @Override
+    public synchronized void onSelfClosed(ClientHandler handler) {
+        clientHandlerList.remove(handler);
+    }
+
+    @Override
+    public void onNewMessageArrived(ClientHandler handler, String msg) {
+        System.out.println("收到—"+handler.getClientInfo()+"--"+msg);
+        //异步提交转发任务
+        forwardingThreadPoolExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (TCPServer.this){
+                    for (ClientHandler clientHandler:clientHandlerList) {
+                        if (clientHandler.equals(handler)){
+                            //跳过自己
+                            continue;
+                        }
+                        //对其它客户端发送消息
+                        clientHandler.send(msg);
+                    }
+                }
+            }
+        });
+
+
+
+    }
+
+    private class ClientListener extends Thread {
+        private boolean done = false;
+
+        @Override
+        public void run() {
+            super.run();
+
+            System.out.println("服务器准备就绪～");
+            // 等待客户端连接
+            do {
+                // 得到客户端
+                try {
+                    if(selector.select() == 0){
+                        if (done){
+                            break;
+                        }
+                        continue;
+                    }
+                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                    while (iterator.hasNext()){
+                        if (done){
+                            break;
+                        }
+                        SelectionKey key = iterator.next();
+                        iterator.remove();
+                        //检查当前key的状态是否是我们关注的
+                        //客户端到达状态
+                        if (key.isAcceptable()){
+                            ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                            //非阻塞状态拿到客户端连接
+                            SocketChannel socketChannel = serverSocketChannel.accept();
+                            try {
+                                // 客户端构建异步线程
+                                ClientHandler clientHandler = new ClientHandler(socketChannel, TCPServer.this);
+                                // 读取数据并打印
+                                clientHandler.readToPrint();
+                                //添加同步处理
+                                synchronized (TCPServer.this) {
+                                    clientHandlerList.add(clientHandler);
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                System.out.println("客户端连接异常：" + e.getMessage());
+                            }
+
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            } while (!done);
+
+            System.out.println("服务器已关闭！");
+        }
+
+        void exit() {
+            done = true;
+            //取消阻塞状态 唤醒当前阻塞
+            selector.wakeup();
+        }
+    }
+}
---
+++ sample-server/src/main/java/net/qiujuer/lesson/sample/server/handle/ClientHandler.java
@@ -0,0 +1,199 @@
+package net.qiujuer.lesson.sample.server.handle;
+
+
+
+import net.qiujuer.library.clink.utils.CloseUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ClientHandler {
+    private final SocketChannel socketChannel;
+    private final ClientReadHandler readHandler;
+    private final ClientWriteHandler writeHandler;
+    private final ClientHandlerCallback clientHandlerCallback;
+    private final String clientInfo;
+
+    public ClientHandler(SocketChannel socketChannel, ClientHandlerCallback clientHandlerCallback) throws IOException {
+        this.socketChannel = socketChannel;
+        socketChannel.configureBlocking(false);//设置非阻塞模式
+        //读
+        Selector readSelector = Selector.open();
+        socketChannel.register(readSelector, SelectionKey.OP_READ);
+        this.readHandler = new ClientReadHandler(readSelector);
+        //写
+        Selector writeSelector = Selector.open();
+        socketChannel.register(writeSelector, SelectionKey.OP_WRITE);
+        this.writeHandler = new ClientWriteHandler(writeSelector);
+
+        this.clientHandlerCallback = clientHandlerCallback;
+        this.clientInfo = socketChannel.getRemoteAddress().toString();//服务端地址
+        System.out.println("新客户端连接：" + clientInfo);
+    }
+
+    public String getClientInfo() {
+        return clientInfo;
+    }
+
+    public void exit() {
+        readHandler.exit();
+        writeHandler.exit();
+        CloseUtils.close(socketChannel);
+        System.out.println("客户端已退出：" + clientInfo);
+    }
+
+    public void send(String str) {
+        writeHandler.send(str);
+    }
+
+    public void readToPrint() {
+        readHandler.start();
+    }
+
+    private void exitBySelf() {
+        exit();
+        clientHandlerCallback.onSelfClosed(this);
+    }
+
+    public interface ClientHandlerCallback {
+        //自身关闭通知
+        void onSelfClosed(ClientHandler handler);
+
+        //收到消息时通知
+        void onNewMessageArrived(ClientHandler handler, String msg);
+    }
+
+    class ClientReadHandler extends Thread {
+        private final Selector selector;
+        private final ByteBuffer byteBuffer;
+        private boolean done = false;
+
+        ClientReadHandler(Selector selector) {
+            this.selector = selector;
+            this.byteBuffer = ByteBuffer.allocate(256);
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            try {
+                do {
+                    // 客户端拿到一条数据
+                    if (selector.select() == 0){
+                        if (done){
+                            break;
+                        }
+                        continue;
+                    }
+
+                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                    while (iterator.hasNext()){
+                        if (done){
+                            break;
+                        }
+                        SelectionKey key = iterator.next();
+                        iterator.remove();
+                        if (key.isReadable()){
+                            SocketChannel client = (SocketChannel) key.channel();
+                            byteBuffer.clear();
+                            int read = client.read(byteBuffer);
+                            if (read>0){
+                                //丢弃换行符
+                                String str = new String(byteBuffer.array(), 0, read - 1);
+                                clientHandlerCallback.onNewMessageArrived(ClientHandler.this, str);
+                            }else {
+                                System.out.println("客户端已无法读取数据！");
+                                // 退出当前客户端
+                                ClientHandler.this.exitBySelf();
+                                break;
+                            }
+                        }
+                    }
+                } while (!done);
+            } catch (Exception e) {
+                if (!done) {
+                    System.out.println("连接异常断开");
+                    ClientHandler.this.exitBySelf();
+                }
+            } finally {
+                // 连接关闭
+                CloseUtils.close(selector);
+            }
+        }
+
+        void exit() {
+            done = true;
+            selector.wakeup();
+            CloseUtils.close(selector);
+        }
+    }
+
+    class ClientWriteHandler {
+        private boolean done = false;
+        private final Selector selector;
+        private final ByteBuffer byteBuffer;
+        private final ExecutorService executorService;
+
+        ClientWriteHandler(Selector selector) {
+            this.selector = selector;
+            byteBuffer = ByteBuffer.allocate(256);
+            this.executorService = Executors.newSingleThreadExecutor();
+        }
+
+        void exit() {
+            done = true;
+            CloseUtils.close(selector);
+            executorService.shutdownNow();
+        }
+
+        void send(String str) {
+            if (done){
+                return;
+            }
+            executorService.execute(new WriteRunnable(str));
+        }
+
+        class WriteRunnable implements Runnable {
+            private final String msg;
+
+            WriteRunnable(String msg) {
+                this.msg = msg + '\n';
+            }
+
+            @Override
+            public void run() {
+                if (ClientWriteHandler.this.done) {
+                    return;
+                }
+                byteBuffer.clear();
+                byteBuffer.put(msg.getBytes());
+                byteBuffer.flip();//反转 将指针恢复到初始位置
+                while (!done&& byteBuffer.hasRemaining()){
+                    try {
+                        int len = socketChannel.write(byteBuffer);
+                        //len=0是合法的
+                        if (len<0){
+                            System.out.printf("客户端无法发送数据--");
+                            ClientHandler.this.exitBySelf();
+                            break;
+                        }
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+    }
+}
